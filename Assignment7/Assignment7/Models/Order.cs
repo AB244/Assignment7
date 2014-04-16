49ed34839dfc3eafa365f0f0af59e708f28268d4
AB244 <ajbenn04@louisville.edu>
2014-04-15 23:38:08 -0400

    Model creation

---
+++ Assignment7/Assignment7/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment7.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Customer { get; set; }
+
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
